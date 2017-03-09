9a8d2ec220d2448d5139a5cbd3c0e1f4945c40ef
Rael Kenny <RaelKenny@gmail.com>
2017-03-09 16:59:19 -0500

    JSON

--- Newz/ArticleListTableViewController.swift
+++ Newz/ArticleListTableViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ArticleListTableViewController: UITableViewController {
 
+    var articles = [Article]()  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getNews(section: "world")
+        
         //this is a custom cell: XIB
         // 1. get the xib (custom cell)
         // 2. register the tableview cell using the xib
@@ -19,13 +23,64 @@ class ArticleListTableViewController: UITableViewController {
         let nib = UINib.init(nibName: "ArticleTableViewCell", bundle: nil) // 1.
         
         self.tableView.register(nib, forCellReuseIdentifier: "articleCell") // 2.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func getNews(section:String){
+        let urlString = "https://api.nytimes.com/svc/topstories/v2/\(section).json?api-key=bdbf3dea91ed4eb388d6e2e823f99f9b"
+        
+        //convert string to URL object .1
+        //create a URL session .2
+        
+        
+        let url = URL(string: urlString)! //1
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in //2
+            
+            guard let myData = data
+                else { return }
+            
+            guard let json = try? JSONSerialization.jsonObject(with: myData, options: []) as! [String:AnyObject]
+                else { return }
+            
+            //get the results array from the JSON dictionary
+            let results = json["results"]! as! [AnyObject]
+            
+            for result in results {
+                
+                guard let title = result["title"] as? String,
+                    let author = result["byline"] as? String,
+                    let abstract = result["abstract"] as? String,
+                    let url = result["url"] as? String,
+                    let date = result["published_date"] as? String
+                    else { continue }
+            
+                guard let link = URL(string:url)
+                    else { continue }
+                
+                guard let multimedia = result["multimedia"] as? [AnyObject]
+                    else { continue }
+                
+                guard multimedia.count > 0
+                    else { continue }
+                
+                let imageMetadata = multimedia.last as! [String: AnyObject]
+                guard let imageURLString = imageMetadata ["url"]! as? String
+                    else { continue }
+                
+                guard let imageURL = URL(string:imageURLString)
+                    else { continue }
+                
+                let newArticle = Article(title: title, author: author, abstract: abstract, date: date, imageURL: imageURL, link: link)
+            } 
+            
+            
+        }.resume()
     }
-
+    
+    
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -38,15 +93,19 @@ class ArticleListTableViewController: UITableViewController {
         return 5
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "articleCell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 160.0
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
--- readme.md
+++ readme.md
@@ -8,3 +8,6 @@ travel
 
 
 https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=bdbf3dea91ed4eb388d6e2e823f99f9b
+
+
+bdbf3dea91ed4eb388d6e2e823f99f9b
